a2af87ca36344c2e90346216815e2bb7faeceec8
Subham Ghosh <subham.ghosh93@gmail.com>
2017-11-20 16:02:31 -0600

    functionality to use jaccard similarity result file added

--- memn2n-master/data_utils.py
+++ memn2n-master/data_utils.py
@@ -78,43 +78,96 @@ def get_stories(f, only_supporting=False):
     with open(f) as f:
         return parse_stories(f.readlines(), only_supporting=only_supporting)
 
-def vectorize_data(data, word_idx, sentence_size, memory_size):
+# def vectorize_data(data, word_idx, sentence_size, memory_size, filter=False):
+#     """
+#     Vectorize stories and queries.
+#
+#     If a sentence length < sentence_size, the sentence will be padded with 0's.
+#
+#     If a story length < memory_size, the story will be padded with empty memories.
+#     Empty memories are 1-D arrays of length sentence_size filled with 0's.
+#
+#     The answer array is returned as a one-hot encoding.
+#     """
+#     S = []
+#     Q = []
+#     A = []
+#     for story, query, answer in data:
+#         ss = []
+#         for i, sentence in enumerate(story, 1):
+#             ls = max(0, sentence_size - len(sentence))
+#             ss.append([word_idx[w] for w in sentence] + [0] * ls)
+#
+#         # take only the most recent sentences that fit in memory
+#         ss = ss[::-1][:memory_size][::-1]
+#
+#         # Make the last word of each sentence the time 'word' which
+#         # corresponds to vector of lookup table
+#         for i in range(len(ss)):
+#             ss[i][-1] = len(word_idx) - memory_size - i + len(ss)
+#
+#         # pad to memory_size
+#         lm = max(0, memory_size - len(ss))
+#         for _ in range(lm):
+#             ss.append([0] * sentence_size)
+#
+#         lq = max(0, sentence_size - len(query))
+#         q = [word_idx[w] for w in query] + [0] * lq
+#
+#         y = np.zeros(len(word_idx) + 1) # 0 is reserved for nil word
+#         for a in answer:
+#             y[word_idx[a]] = 1
+#
+#         S.append(ss)
+#         Q.append(q)
+#         A.append(y)
+#     return np.array(S), np.array(Q), np.array(A)
+
+
+def vectorize_data(data, word_idx, sentence_size, memory_size,  filter=False):
     """
     Vectorize stories and queries.
-
     If a sentence length < sentence_size, the sentence will be padded with 0's.
-
     If a story length < memory_size, the story will be padded with empty memories.
     Empty memories are 1-D arrays of length sentence_size filled with 0's.
-
     The answer array is returned as a one-hot encoding.
     """
     S = []
     Q = []
     A = []
     for story, query, answer in data:
+        lq = max(0, sentence_size - len(query))
+        q = [word_idx[w] for w in query] + [0] * lq
+
         ss = []
         for i, sentence in enumerate(story, 1):
             ls = max(0, sentence_size - len(sentence))
             ss.append([word_idx[w] for w in sentence] + [0] * ls)
 
-        # take only the most recent sentences that fit in memory
-        ss = ss[::-1][:memory_size][::-1]
+        if len(ss) > memory_size:
+            if filter == True:
+                # Use Jaccard similarity to determine the most relevant sentences
+                q_words = set(q)
+                least_like_q = sorted(ss, cmp=lambda x, y: jaccard(set(x), q_words) < jaccard(set(y), q_words))[
+                               :len(ss) - memory_size]
+                for sent in least_like_q:
+                    # Remove the first occurrence of sent. A list comprehension as in [sent for sent in ss if sent not in least_like_q]
+                    # should not be used, as it would remove multiple occurrences of the same sentence, some of which might actually make the cutoff.
+                    ss.remove(sent)
+            else:
+                # take only the most recent sentences that fit in memory
+                ss = ss[::-1][:memory_size][::-1]
 
-        # Make the last word of each sentence the time 'word' which 
-        # corresponds to vector of lookup table
+        else:
+            # pad to memory_size
+            lm = max(0, memory_size - len(ss))
+            for _ in range(lm):
+                ss.append([0] * sentence_size)
+
         for i in range(len(ss)):
             ss[i][-1] = len(word_idx) - memory_size - i + len(ss)
 
-        # pad to memory_size
-        lm = max(0, memory_size - len(ss))
-        for _ in range(lm):
-            ss.append([0] * sentence_size)
-
-        lq = max(0, sentence_size - len(query))
-        q = [word_idx[w] for w in query] + [0] * lq
-
-        y = np.zeros(len(word_idx) + 1) # 0 is reserved for nil word
+        y = np.zeros(len(word_idx) + 1)  # 0 is reserved for nil word
         for a in answer:
             y[word_idx[a]] = 1
 
@@ -122,3 +175,11 @@ def vectorize_data(data, word_idx, sentence_size, memory_size):
         Q.append(q)
         A.append(y)
     return np.array(S), np.array(Q), np.array(A)
+
+
+def jaccard(a, b):
+    '''
+    Assumes that a and b are sets so that calling code only has to cast the question to set once.
+    '''
+    return len(a.intersection(b)) / float(len(a.union(b)))
+    set(a).intersection(set(b))
